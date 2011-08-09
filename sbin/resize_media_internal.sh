b34d07d12e035de038a2aaa5a0ef63840b377866
tking <tking@flik.(none)>
2011-08-09 01:12:21 -0700

    finished the logic for increasing or reducing the media_internal partition (including verification logic) + user feedback

--- sbin/resize_media_internal.sh
+++ sbin/resize_media_internal.sh
@@ -1,24 +1,50 @@
 #!bin/sh
 
 # $1 is new requested size
-# $2 is requested size + 100MB
+requested_size = "$1"
+# $2 is requested size + 100MBa
+existing_size = "$2"
+# space so we dont' run into the wall
+requested_fs_size = requested_size - 100
 
-echo 'stop crpytofs'
-pkill -SIGUSR1 cryptofs
-echo 'unmount so we can fsck the partition before we resize'
-umount /media/internal
-echo 'check vfat fs before we attempt resize and fix'
-/usr/sbin/fsck.vfat /dev/mapper/store-media 
-echo 'resize vfat fs to new target size'
-resizefat -p  /dev/mapper/store-media $1G
-echo 'check vfat fs after resize'
-/usr/sbin/fsck.vfat /dev/mapper/store-media 
-echo 'reduce the size of the LV'
-lvreduce -L $2G /dev/mapper/store-media
-echo 'check vfat fs after lvreduce'
-/usr/sbin/fsck.vfat /dev/mapper/store-media 
-echo 'remount /media/internal'
-mount -t vfat /dev/mapper/store-media /media/internal
-echo 'restart cryptofs'
-pkill -SIGUSR2 cryptofs
-echo 'all done!'
+if [ ${REQUESTED_SIZE} > ${EXISTING_SIZE} ]
+    echo 'stop crpytofs'
+    pkill -SIGUSR1 cryptofs
+    echo 'unmount so we can fsck the partition before we resize'
+    umount /media/internal
+    echo 'check vfat fs before we attempt resize and fix'
+    /usr/sbin/fsck.vfat /dev/mapper/store-media 
+    lvresize -L ${TARGET_SIZE}M /dev/mapper/store-media
+    echo 'check vfat fs after lvrsize'
+    /usr/sbin/fsck.vfat /dev/mapper/store-media
+    echo 'resize vfat fs to new target size'
+    resizefat -p  /dev/mapper/store-media ${REQUESTED_FS_SIZE}M
+    echo 'check vfat fs after resize'
+    /usr/sbin/fsck.vfat /dev/mapper/store-media 
+    echo 'remount /media/internal'
+    mount -t vfat /dev/mapper/store-media /media/internal
+    echo 'restart cryptofs'
+    pkill -SIGUSR2 cryptofs
+    echo 'all done!'
+else
+    echo 'stop crpytofs'
+    pkill -SIGUSR1 cryptofs
+    echo 'unmount so we can fsck the partition before we resize'
+    umount /media/internal
+    echo 'check vfat fs before we attempt resize and fix'
+    /usr/sbin/fsck.vfat /dev/mapper/store-media
+    echo 'resize vfat fs to new target size'
+    resizefat -p  /dev/mapper/store-media ${REQUESTED_FS_SIZE}G
+    echo 'check vfat fs after resize'
+    /usr/sbin/fsck.vfat /dev/mapper/store-media
+     echo 'reduce the size of the LV'
+    lvreduce -L ${REQUESTED_SIZE}M /dev/mapper/store-media
+    echo 'check vfat fs after lvreduce'
+    /usr/sbin/fsck.vfat /dev/mapper/store-media
+    echo 'remount /media/internal'
+    mount -t vfat /dev/mapper/store-media /media/internal
+    echo 'restart cryptofs'
+    pkill -SIGUSR2 cryptofs
+    echo 'all done!'
+fi
+
