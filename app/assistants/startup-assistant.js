cea079ac169efcc7749db06b8f6f6a1486842b28
Rod Whitby <rod@whitby.id.au>
2011-10-24 20:18:22 +1030

    Fixed the post-install and pre-remove scripts

--- app/assistants/startup-assistant.js
+++ app/assistants/startup-assistant.js
@@ -11,6 +11,7 @@ function StartupAssistant(changelog)
     this.newMessages =
 	[
 	 // Don't forget the comma on all but the last entry
+	 { version: '0.0.5', log: [ 'Fixed the post-install and pre-remove scripts' ] },
 	 { version: '0.0.4', log: [ 'Added the ability to unmount items from inside jails' ] },
 	 { version: '0.0.3', log: [ 'Highlight selected partition, and update filesystem sizes during simulations' ] },
 	 { version: '0.0.2', log: [ 'Alpha release to check UI workflows and installation pipecleaning' ] },
